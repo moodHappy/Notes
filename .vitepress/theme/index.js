de88e0984247c142e1d3230b5474638fe0e7e676
moodHappy <lhwuhuei@gmail.com>
2026-08-06 11:27:16 +0800

    更新 index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,10 +1,18 @@
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
 import WebEditor from './components/WebEditor.vue'
+import FloatingButton from './components/FloatingButton.vue'
 
 export default {
   ...DefaultTheme,
+  // 核心修复：利用 Layout 插槽，强行把你的悬浮按钮注入到整个网站的最外层
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'layout-top': () => h(FloatingButton)
+    })
+  },
   enhanceApp({ app }) {
-    // 注册全局组件
+    // 保持你的编辑器组件注册不变
     app.component('WebEditor', WebEditor)
   }
 }
